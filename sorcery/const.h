493ca163020a756c7bbe3c0768069f7299e50377
TJ <amuro95@outlook.com>
2017-04-01 22:59:34 -0400

    strings

--- sorcery/const.h
+++ sorcery/const.h
@@ -1,4 +1,8 @@
 #ifndef _CONST_H_
 #define _CONST_H_
 extern const int maxPop = 5;
+std::string SoT = "START";
+std::string EoT = "END";
+std::string M_in = "MinionEnters";
+std::string M_out = "MinionLeaves";
 #endif
